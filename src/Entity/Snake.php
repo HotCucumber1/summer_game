54e2eaedfe6469ba59298ff1e495b3fdb349eee5
Yuri-Sokolov <syv.info@mail.ru>
2024-06-25 10:55:07 +0300

    Описал сущность змеи

--- src/Entity/Snake.php
+++ src/Entity/Snake.php
@@ -4,5 +4,42 @@ namespace App\Entity;
 
 class Snake
 {
+    public function __construct(
+        private int $headX,
+        private int $headY,
+        private array $bodyParts
+    )
+    
+    {
+    }
 
+    public function getHeadX(): int
+    {
+        return $this->headX;
+    }
+
+    public function getHeadY(): int
+    {
+        return $this->headY;
+    }
+
+    public function getBodyParts(): array
+    {
+        return $this->bodyParts;
+    }
+
+    public function setHeadX(int $headX): void
+    {
+        $this->headX = $headX;
+    }
+
+    public function setHeadY(int $headY): void
+    {
+        $this->headY = $headY;
+    }
+
+    public function setBodyParts(array $bodyParts): void
+    {
+        $this->bodyParts = $bodyParts;
+    }
 }
