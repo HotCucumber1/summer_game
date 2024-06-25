caba06d9b702df536a5d9fcc82be9792427f5105
hotcucumber <shadowpersik05@mail.ru>
2024-06-25 10:37:05 +0300

    Описал сущность пользователя

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,5 +4,66 @@ namespace App\Entity;
 
 class User
 {
+    public function __construct(private int $id,
+                                private string $name, // unique
+                                private ?string $password,
+                                private int $score)
+    {
+    }
 
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @return int
+     */
+    public function getScore(): int
+    {
+        return $this->score;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @param string|null $password
+     */
+    public function setPassword(?string $password): void
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @param int $score
+     */
+    public function setScore(int $score): void
+    {
+        $this->score = $score;
+    }
 }
