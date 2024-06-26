736a7e5f7a2a97d00277c2be9b4edfbd40499225
hotcucumber <shadowpersik05@mail.ru>
2024-06-26 12:52:17 +0300

    [snake_bumps_task] Реализовал коллизию стенки, добавил сущность Wall для взаимодействия с ней

---
+++ src/Entity/Wall.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Entity;
+
+class Wall
+{
+    const X = 0;
+    const Y = 0;
+    const RADIUS = 1000;
+}
--- src/Service/CollisionService.php
+++ src/Service/CollisionService.php
@@ -2,7 +2,32 @@
 
 namespace App\Service;
 
+use App\Entity\Snake;
+use App\Entity\Wall;
+
 class CollisionService
 {
+    public function __construct()
+    {
+    }
 
+    public function isWallBump(Snake $snake): bool
+    {
+        $snakeX = $snake->getHeadX();
+        $snakeY = $snake->getHeadY();
+        $snakeR = $snake->getRadius();
+
+        $dist = sqrt(($snakeX - Wall::X) * ($snakeX - Wall::X) + ($snakeY - Wall::Y) * ($snakeY - Wall::Y));
+        $sin = $snakeY / $dist;
+        $cos = $snakeX / $dist;
+
+        $bumpX = abs($snakeX) + $snakeR * abs($cos);
+        $bumpY = abs($snakeY) + $snakeR * abs($sin);
+
+        if ($bumpX * $bumpX + $bumpY * $bumpY >= Wall::RADIUS ** Wall::RADIUS)
+        {
+            return true;
+        }
+        return false;
+    }
 }
